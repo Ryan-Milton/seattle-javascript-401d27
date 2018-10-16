458f7bd40f16f8657c49b65bba2e2e0aa34fd2ca
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-10-16 13:48:43 -0700

    package.json changes

--- 01-node-ecosystem/lab/lib/greet.js
+++ 01-node-ecosystem/lab/lib/greet.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function greet(str){
-    if(str === undefined){
-        return 'hello world';
-    }
-    if(typeof str !== 'string'){
-        return null;
-    }
-    if(str){
-        return `hello ${str}`;
-    } 
+  if(str === undefined){
+    return 'hello world';
+  }
+  if(typeof str !== 'string'){
+    return null;
+  }
+  if(str){
+    return `hello ${str}`;
+  } 
     
 }
 greet('Ryan');
