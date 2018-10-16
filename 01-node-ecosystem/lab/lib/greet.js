3e3a87d50bcf1c7654d95e9eca1fc741e07ef83e
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-10-15 21:29:39 -0700

    lab-01 is complete

---
+++ 01-node-ecosystem/lab/lib/greet.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function greet(str){
+    if(str === undefined){
+        return 'hello world';
+    }
+    if(typeof str !== 'string'){
+        return null;
+    }
+    if(str){
+        return `hello ${str}`;
+    } 
+    
+}
+greet('Ryan');
+module.exports=greet;
