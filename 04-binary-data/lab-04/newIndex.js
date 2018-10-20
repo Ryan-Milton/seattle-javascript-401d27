5dcc2fe7f07c75a105685277ec8702764a14a9a4
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-10-19 17:13:27 -0700

    greyscale and overwrite complete

--- 04-binary-data/lab-04/newIndex.js
+++ 04-binary-data/lab-04/newIndex.js
@@ -28,9 +28,20 @@ const copyPaste = (bmp) => {
   console.log('Copy Pasta! Hot and ready!.');
 };
 
+const overCast = (bmp) => {
+  console.log('This is how you look on a typical Seattle day.');
+  for(let i = 122; i < 1146; i += 4) {
+    let avg = (bmp.buffer[i] + bmp.buffer[i + 1] + bmp.buffer[i + 2]) / 3;
+    bmp.buffer[i] = avg;
+    bmp.buffer[i +1] = avg;
+    bmp.buffer[i +2] = avg;
+  }
+};
+
 const overWrite = (bmp) => {
   console.log('The pen is mighty!');
-  bmp.buffer.write('I\'m overwriting your file.');
+  // bmp.buffer.write('I\'m overwriting your file.');
+  bmp.buffer = Buffer.from('You\'re file has been overwritten!');
 };
 
 //an object containing different methods for transformation
@@ -38,6 +49,7 @@ const transformDictionary = {
   //TODO: Make a greyscale transformation function in the future
   copy: copyPaste,
   write: overWrite,
+  seattle: overCast,
 };
 
 function readFileAndTransform() {
