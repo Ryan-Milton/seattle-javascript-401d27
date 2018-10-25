d44a3f83caa3c1333b1855942b897590f8f6c455
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-10-24 18:06:31 -0700

    minor changes

--- 08-http-routing/lab/src/api/api.js
+++ 08-http-routing/lab/src/api/api.js
@@ -2,7 +2,8 @@
 
 const router = require('../lib/router.js');
 
-router.get('/', (request, response) => {
+router.get('/api/v1/notes', (request, response) => {
+  //http://localhost:3000/?name=<YOUR_NAME_HERE>
   response.statusCode = 200;
   response.statusMessage = 'OK';
   let name = request.query.name || '';
@@ -10,9 +11,20 @@ router.get('/', (request, response) => {
   response.end();
 });
 
-router.post('/data', (request, response) => {
+router.post('/api/v1/notes', (request, response) => {
+  //http://localhost:3000/data
+  //don't forget to add JSON in te Body.raw section of Postman
   response.statusCode = 200;
   response.statusMessage = 'OK';
+  response.write( JSON.stringify(request.body) );
+  response.end();
+});
+
+router.put('/data/update', (request, response) => {
+  //http://localhost:3000/update?id=<ID_HERE>
+  response.statusCode = 200;
+  response.statusMessage = 'OK';
+  // let id = request.query.id || '';
   response.write( JSON.stringify(request.body) );
   response.end();
 });
