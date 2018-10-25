b7e827a0d2175682379b58d79ecc3e5160ad8ef9
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-10-24 21:44:57 -0700

    lab complete, I think. Not using jest

--- 08-http-routing/lab/src/api/api.js
+++ 08-http-routing/lab/src/api/api.js
@@ -3,7 +3,7 @@
 const router = require('../lib/router.js');
 
 router.get('/api/v1/notes', (request, response) => {
-  //http://localhost:3000/?name=<YOUR_NAME_HERE>
+  //http://localhost:3000/api/v1/notes?name=<YOUR_NAME_HERE>
   response.statusCode = 200;
   response.statusMessage = 'OK';
   let name = request.query.name || '';
@@ -12,7 +12,7 @@ router.get('/api/v1/notes', (request, response) => {
 });
 
 router.post('/api/v1/notes', (request, response) => {
-  //http://localhost:3000/data
+  //http://localhost:3000/api/v1/notes
   //don't forget to add JSON in te Body.raw section of Postman
   response.statusCode = 200;
   response.statusMessage = 'OK';
@@ -20,12 +20,21 @@ router.post('/api/v1/notes', (request, response) => {
   response.end();
 });
 
-router.put('/data/update', (request, response) => {
-  //http://localhost:3000/update?id=<ID_HERE>
+router.put('/api/v1/notes', (request, response) => {
+  //http://localhost:3000/api/v1/notes?id=<ID_HERE>
   response.statusCode = 200;
   response.statusMessage = 'OK';
-  // let id = request.query.id || '';
-  response.write( JSON.stringify(request.body) );
+  let id = request.query.id || '';
+  response.write( `ID: ${id} was requested.` );
+  response.end();
+});
+
+router.delete('/api/v1/notes', (request, response) => {
+  ////http://localhost:3000/api/v1/notes?id=<ID_HERE>
+  response.statusCode = 200;
+  response.statusMessage = 'OK';
+  let id = request.query.id || '';
+  response.write( `ID: ${id} was deleted.`);
   response.end();
 });
 
