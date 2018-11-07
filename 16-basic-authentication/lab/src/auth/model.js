a8bc5e892cbdd1b828138fa2aea3ab34487a15b7
Ryan-Milton <mr.ryan.milton@gmail.com>
2018-11-07 14:59:33 -0800

    basic auth complete

--- 16-basic-authentication/lab/src/auth/model.js
+++ 16-basic-authentication/lab/src/auth/model.js
@@ -1,3 +1,4 @@
+'use strict';
 
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
@@ -5,7 +6,7 @@ import jwt from 'jsonwebtoken';
 
 const schema = new Schema({
   username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
+  // email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
 });
 
@@ -17,7 +18,22 @@ const schema = new Schema({
 //   });
 // });
 
+schema.pre('save', function(next) {
+  bcrypt.hash(this.password, 10)
+    .then(hashedPassword => {
+      this.password = hashedPassword;
+      next();
+    })
+    .catch( error => { throw error; } );
+});
 // ------------ Found a tutorial and am trying to following it ----------------------
+
+schema.statics.authenticate = function(auth) {
+  let query = {username:auth.username};
+  return this.findOne(query)
+    .then(user => user && user.comparePassword(auth.password))
+    .catch(error => error);
+};
 
 schema.methods.generateHash = function(password) {
   return bcrypt.hash(password, 10);
@@ -25,24 +41,18 @@ schema.methods.generateHash = function(password) {
 
 // Generate a JWT from the user id and a secret
 schema.methods.generateToken = function() {
-  const token = jwt.sign({id:this._id}, process.env.APP_SECRET); 
-  console.log(token);
-  return token;
+  return jwt.sign( {id:this._id}, process.env.APP_SECRET);
 };
 
 // Compare a plain text password against the hashed one we have saved
 schema.methods.comparePassword = function(password) {
-  return bcrypt.whassup(password, this.password);
+  return bcrypt.compare(password, this.password)
+    .then(valid => valid ? this : null);
 };
 
 // If we got a user/password, compare them to the hashed password
 // If we got a token, validate it and then pull the user id
 // In both cases, return the user instance or an error
-schema.statics.authenticate = function(auth) {
-  let query = {'idont':'know'};
-  return this.findOne(query)
-    .then(user => user && user.comparePassword(auth.password))
-    .catch(error => error);
-};
 
-export default mongoose.model('User', schema);
+
+export default mongoose.model('users', schema);
